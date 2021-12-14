5b878dbcb5022bf20dbbcd228efb36b212c0c152
StefGeraets <stef.geraets@gmail.com>
2021-12-14 13:48:04 +0100

    Day 2

---
+++ day-2/index.js
@@ -0,0 +1,47 @@
+import { test, real } from "./data.js";
+
+const result = {
+  horizontal: 0,
+  depth: 0,
+}
+
+const directions = (array) => {
+  return array.reduce((map, obj) => {
+    for (let key in obj) {
+      switch (key) {
+        case "forward":
+          return { 
+            horizontal: map.horizontal += obj[key], 
+            depth: map.depth + (map.aim * obj[key]), 
+            aim: map.aim 
+          }
+        case "down":
+          return { 
+            horizontal: map.horizontal, 
+            depth: map.depth, 
+            aim: map.aim += obj[key]
+          }
+        case "up":
+          return { 
+            horizontal: map.horizontal, 
+            depth: map.depth,
+            aim: map.aim -= obj[key]
+          }
+        default:
+          break;
+      }
+    }
+  }, {
+    horizontal: 0,
+    depth: 0,
+    aim: 0
+  })
+}
+
+const multiplyObject = (object) => {
+  let numbers = Object.values(object);
+  return numbers[0] * numbers[1];
+}
+
+console.log(directions(test))
+console.log(multiplyObject(directions(test)))
